45e50786c986b273a54890d6bb1396a4bf717cad
caohaijing <caohaijing1@163.com>
2024-12-05 13:19:55 +0800

    test

---
+++ src/main/java/com/chj/helloworld/HelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.chj.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot启动类 入口
+ */
+@SpringBootApplication
+public class HelloworldApplication {
+	//启动springboot应用
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
